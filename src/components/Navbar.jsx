703e753086c8a43f4707959ff395f85f20708c0d
josefnaranjo <jfnaappus@gmail.com>
2023-11-20 20:04:40 -0800

    v1.1 - Added Navbar, GetStarted, and Hero

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,53 @@
 import React from 'react'
+import { useState } from 'react'  // Hook
 
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 function Navbar() {
+  const [toggle, setToggle] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='w-full flex py-6 justify-between items-center navbar'>
+      <img src= {logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+      {navLinks.map((nav, index) => (
+          <li
+          key={nav.id}
+          className={`font-poppins font-normal cursor-pointer 
+          text-[16px] ${index === navLinks.length - 1 ? 
+            'mr-0' : 'mr-10'} text-white`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img src={toggle ? close : menu} 
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prevState) => !prevState)}
+        />
+
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+            <li
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer 
+              text-[16px] ${index === navLinks.length - 1 ? 
+              'mr-0' : 'mb-4'} text-white`}
+            >
+              <a href={`#${nav.id}`}>
+                {nav.title}
+              </a>
+            </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
