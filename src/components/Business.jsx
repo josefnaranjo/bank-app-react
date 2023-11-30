f2f8caadfe116460a83fb3772f94658a8fecc9bc
josefnaranjo <jfnaappus@gmail.com>
2023-11-29 19:06:01 -0800

    v1.5 - Modified navbar to be fixed at the top

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -8,7 +8,7 @@ const FeatureCard = ({icon, title, content, index}) => (
   <div className={`flex flex-row p-6 rounded-[20px] 
                    ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
-      <img src={icon} alt='icon' className='w-[50%] h-{50%] object-contain' />
+      <img src={icon} alt='icon' className='w-[50%] h-[50%] object-contain' />
     </div>
 
     <div className='flex-1 flex flex-col ml-3'>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,36 +5,35 @@ import { close, logo, menu } from '../assets'
 import { navLinks } from '../constants'
 function Navbar() {
   const [toggle, setToggle] = useState(false)
+  const [isNavbarFixed, setIsNavbarFixed] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const navbar = document.querySelector('.navbar');
-      if (navbar) {
-        const offset = window.scrollY;
-        if (offset > 0) {
-          navbar.classList.add('navbar-fixed');
-        } else {
-          navbar.classList.remove('navbar-fixed');
-        }
+      const offset = window.scrollY;
+      if (offset > 0 && !isNavbarFixed) {
+        setIsNavbarFixed(true);
+      } else if (offset === 0 && isNavbarFixed) {
+        setIsNavbarFixed(false);
       }
     };
 
     window.addEventListener('scroll', handleScroll);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [isNavbarFixed]);
 
   return (
-    <div className='w-full flex py-6 justify-between items-center navbar'>
+    <div className={`w-full flex py-6 justify-between items-center ${isNavbarFixed ? 'fixed top-0 bg-black-gradient shadow-md' : ''} z-[10]`} style={{ width: '50vw', maxWidth: '100%', padding: '15px' }}>
       <img src= {logo} alt='hoobank' className='w-[124px] h-[32px]' />
-      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1 links'>
       {navLinks.map((nav, index) => (
           <li
           key={nav.id}
           className={`font-poppins font-normal cursor-pointer 
           text-[16px] ${index === navLinks.length - 1 ? 
-            'mr-0' : 'mr-10'} text-white`}
+            'mr-0' : 'mr-10'} text-white hover:text-secondary`}
           >
             <a href={`#${nav.id}`}>
               {nav.title}
